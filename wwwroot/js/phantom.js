048292ef16c7f33a04889c03c434e8a46ce55a80
Armando Fernandez <codefilez@gmail.com>
2025-03-04 14:19:42 -0800

    Added signTransaction handling - phantom.js

--- wwwroot/js/phantom.js
+++ wwwroot/js/phantom.js
@@ -29,5 +29,22 @@
             console.warn("Phantom Wallet is not installed.");
             return false;
         }
+    },
+
+    signTransaction: async function (transactionBase64) {
+        if (window.solana && window.solana.isPhantom) {
+            try {
+                const transactionBuffer = new Uint8Array(atob(transactionBase64).split("").map(c => c.charCodeAt(0)));
+                const transaction = solanaWeb3.Transaction.from(transactionBuffer);
+                const signedTransaction = await window.solana.signTransaction(transaction);
+                return btoa(signedTransaction.serialize());
+            } catch (err) {
+                console.error("Error signing transaction:", err);
+                return null;
+            }
+        } else {
+            console.warn("Phantom Wallet is not installed.");
+            return null;
+        }
     }
 };
