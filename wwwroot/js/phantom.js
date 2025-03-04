45d5554dadc519f1af5e1198ca147a019c9d2204
Armando Fernandez <codefilez@gmail.com>
2025-03-04 12:10:07 -0800

    Phantom component & Radzen Framework Implemented.

---
+++ wwwroot/js/phantom.js
@@ -0,0 +1,16 @@
+﻿window.phantomWallet = {
+    connect: async function () {
+        if (window.solana && window.solana.isPhantom) {
+            try {
+                const response = await window.solana.connect();
+                return response.publicKey.toString();
+            } catch (err) {
+                console.error("Phantom Wallet connection failed:", err);
+                return null;
+            }
+        } else {
+            alert("Phantom Wallet not found! Please install it.");
+            return null;
+        }
+    }
+};
