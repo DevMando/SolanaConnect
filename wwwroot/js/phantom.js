721aacf0d1b2acdfe2156706d545657369acfe56
Armando Fernandez <codefilez@gmail.com>
2025-03-04 12:46:41 -0800

    Completed Connect/Disconnect Phanthom wallet.

--- wwwroot/js/phantom.js
+++ wwwroot/js/phantom.js
@@ -12,5 +12,22 @@
             alert("Phantom Wallet not found! Please install it.");
             return null;
         }
+    },
+
+    disconnect: async function () {
+        if (window.solana && window.solana.isPhantom) {
+            try {
+                await window.solana.disconnect();
+                window.solana = null;
+                console.log("Phantom Wallet disconnected.");
+                return true;
+            } catch (err) {
+                console.error("Error disconnecting Phantom Wallet:", err);
+                return false;
+            }
+        } else {
+            console.warn("Phantom Wallet is not installed.");
+            return false;
+        }
     }
 };
